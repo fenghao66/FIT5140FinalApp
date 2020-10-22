b97a283ac3795633eff08fa70949d79a5d77f152
fenghao qi <fqii0002@student.monash.edu>
2020-10-22 23:16:36 +1100

    Add scroll view

--- FIT5140FinalApp/HomeViewController.swift
+++ FIT5140FinalApp/HomeViewController.swift
@@ -63,15 +63,20 @@ class HomeViewController: UIViewController {
     }
     
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+//        if segue.identifier == "movieDetailSegue" {
+//            let destination = segue.destination as! MovieDetailViewController
+//            destination.delegate = self
+//        }
+        
     }
-    */
+    
     
     // MARK: - Web request
     
@@ -142,6 +147,12 @@ extension HomeViewController: UICollectionViewDataSource {
             cell.posterImage.downloadImage(imageURLString: movie.posterPath!)
         }
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let controller = self.storyboard?.instantiateViewController(identifier: "movieDetail") as! MovieDetailViewController
+        self.navigationController?.pushViewController(controller, animated: true)
     }
 }
 
---
+++ FIT5140FinalApp/MovieDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MovieDetailViewController.swift
+//  FIT5140FinalApp
+//
+//  Created by Qfh on 22/10/20.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
