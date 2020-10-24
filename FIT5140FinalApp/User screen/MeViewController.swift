9cfcc68470891e117223c4fcba5e4c5961314651
ChengGuang Li <cristianoronando777@gmail.com>
2020-10-24 17:15:17 +1100

    add see more function

--- FIT5140FinalApp/User screen/MeViewController.swift
+++ FIT5140FinalApp/User screen/MeViewController.swift
@@ -11,23 +11,28 @@ import Firebase
 
 class MeViewController: UIViewController {
     
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var view1: UIView!
     @IBOutlet weak var logOutButton: UIButton!
     @IBOutlet weak var firstNameLabel: UILabel!
     @IBOutlet weak var favoriteView: UICollectionView!
     @IBOutlet weak var userImage: UIImageView!
     
+    @IBOutlet weak var seeMoreButton: UIBarButtonItem!
     @IBOutlet weak var watchedButton: UIButton!
     
+    @IBOutlet weak var seeMoreCollectionView: UICollectionView!
     @IBOutlet weak var favoriteButton: UIButton!
     var favoriet:[Int] = [Int]()
     var watched:[Int] = [Int]()
     var movieCollection:[MovieDetailNew] = [MovieDetailNew]()
     var movieDEtail:[String] = [String]()
+    var seeMoreBoolean:Bool = true
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let layout = UICollectionViewFlowLayout()
-//        layout.scrollDirection = .horizontal
-//        self.favoriteView.collectionViewLayout = layout
+        //        let layout = UICollectionViewFlowLayout()
+        //        layout.scrollDirection = .horizontal
+        //        self.favoriteView.collectionViewLayout = layout
         userImage.image = #imageLiteral(resourceName: "user_click")
         
         // costom the tabBar item
@@ -36,14 +41,45 @@ class MeViewController: UIViewController {
         Utilities.signOutButton(logOutButton)
         favoriteButton.setTitleColor(UIColor.red, for: .normal)
         watchedButton.setTitleColor(UIColor.gray, for: .normal)
-      
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
-     getUserData()
-     print("!!!!!! movie Collection \(self.movieCollection.count)")
-      
-    
+        getUserData()
+        print("!!!!!! movie Collection \(self.movieCollection.count)")
+        seeMoreCollectionView.isHidden = true
+        
+    }
+    @IBAction func seeMoreAction(_ sender: Any) {
+        if seeMoreBoolean{
+            seeMoreButton.title = "Back"
+            seeMoreCollectionView.isHidden = false
+            favoriteView.isHidden = true
+            logOutButton.isHidden = true
+            userImage.isHidden = true
+            firstNameLabel.isHidden = true
+            favoriteButton.isHidden = true
+            watchedButton.isHidden = true
+            view1.isHidden = true
+            view2.isHidden = true
+            seeMoreBoolean = false
+            seeMoreCollectionView.reloadData()
+        }else{
+            
+            seeMoreButton.title = "See More"
+            seeMoreBoolean = true
+            seeMoreCollectionView.isHidden = true
+            favoriteView.isHidden = false
+            logOutButton.isHidden = false
+            userImage.isHidden = false
+            firstNameLabel.isHidden = false
+            favoriteButton.isHidden = false
+            watchedButton.isHidden = false
+            view1.isHidden = false
+            view2.isHidden = false
+            favoriteView.reloadData()
+        }
+        
     }
     
     
@@ -69,10 +105,10 @@ class MeViewController: UIViewController {
                         print("@@@@@@@@@@@@@@@@@@@@@@@@@")
                     }else{
                         self.refreshMovieDetailCollection(idCollection: self.favoriet)
-                      
+                        
                         print("#################\(self.favoriet[0])")
                     }
-                                  
+                    
                 }else{
                     
                     print("get data error: \(String(describing: error))")
@@ -109,10 +145,10 @@ class MeViewController: UIViewController {
     }
     
     func getMovieDetailAccordingToMovieId(id: Int){
-       movieCollection = [MovieDetailNew]()
+        movieCollection = [MovieDetailNew]()
         let searchString = Constants.REQUEST_STRING + "/movie/\(id)?api_key=" + Constants.apiKey + "&append_to_response=videos"
         let jsonURL = URL(string: searchString.addingPercentEncoding(withAllowedCharacters:
-                .urlQueryAllowed)!)
+            .urlQueryAllowed)!)
         print(jsonURL ?? " jsonUrl failed")
         let task = URLSession.shared.dataTask(with: jsonURL!) {
             (data, response, error) in
@@ -123,13 +159,13 @@ class MeViewController: UIViewController {
             }
             do{
                 let decoder = JSONDecoder()
-          let objectData = try decoder.decode(MovieDetailNew.self, from: data!)
-
+                let objectData = try decoder.decode(MovieDetailNew.self, from: data!)
+                
                 self.movieCollection.append(objectData)
                 DispatchQueue.main.async {
                     self.favoriteView.reloadData()
                 }
-             
+                
             }catch let error{
                 print(error)
             }
@@ -169,14 +205,38 @@ class MeViewController: UIViewController {
 extension MeViewController: UICollectionViewDataSource,UICollectionViewDelegate{
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-      
-
-      
-     return  movieCollection.count
-     
+        if (collectionView == seeMoreCollectionView){
+            print("1000000000000000000000000")
+            return movieCollection.count
+        }
+        
+        return  movieCollection.count
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if (collectionView == seeMoreCollectionView){
+            let cell1 = collectionView.dequeueReusableCell(withReuseIdentifier: "seeMoreCollectionCell", for: indexPath) as! SeeMoreCollectionViewCell
+            cell1.layer.cornerRadius = 10.0
+            cell1.layer.shadowOpacity = 0.5
+            cell1.layer.shadowRadius = 5
+            cell1.layer.masksToBounds = false
+            let movieDetail1 = movieCollection[indexPath.row]
+            if movieDetail1.backdrop_path == nil {
+                cell1.seeMoreImage.image = UIImage(named: "Image_not_found")
+            }else{
+                
+            cell1.seeMoreImage.downloadPoster(imageURLString: movieDetail1.backdrop_path!)
+            }
+            
+            cell1.seeMoreTitleLabel.text = movieDetail1.title!
+            cell1.seeMoreVoting.text = "\(movieDetail1.voteAvg!)"
+            
+            return cell1
+            
+        }
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath) as! CollectionCollectionViewCell
         
         cell.layer.cornerRadius = 10.0
@@ -209,19 +269,19 @@ extension MeViewController: UICollectionViewDelegateFlowLayout {
 }
 
 extension UIImageView{
-
- func downloadPoster(imageURLString: String) {
     
-    let imageURL = URL(string: "https://image.tmdb.org/t/p/w500\(imageURLString)")!
-    let task = URLSession.shared.dataTask(with: imageURL) {
-        (data, response, error) in
+    func downloadPoster(imageURLString: String) {
+        
+        let imageURL = URL(string: "https://image.tmdb.org/t/p/w500\(imageURLString)")!
+        let task = URLSession.shared.dataTask(with: imageURL) {
+            (data, response, error) in
             
-        if error != nil{
+            if error != nil{
                 print(error!)
                 return
             }
-        DispatchQueue.main.async {
-            self.image = UIImage(data: data!)!
+            DispatchQueue.main.async {
+                self.image = UIImage(data: data!)!
             }
         }
         task.resume()
---
+++ FIT5140FinalApp/User screen/SeeMoreCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SeeMoreCollectionViewCell.swift
+//  FIT5140FinalApp
+//
+//  Created by chengguang li on 2020/10/24.
+//
+
+import UIKit
+
+class SeeMoreCollectionViewCell: UICollectionViewCell {
+    
+   
+    @IBOutlet weak var seeMoreVoting: UILabel!
+    @IBOutlet weak var seeMoreTitleLabel: UILabel!
+    @IBOutlet weak var seeMoreImage: UIImageView!
+}
