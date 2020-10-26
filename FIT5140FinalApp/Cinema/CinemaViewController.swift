ec17f7e315fad27b9f2354755fbb946fdb37a332
ChengGuang Li <cristianoronando777@gmail.com>
2020-10-26 13:44:34 +1100

    can get nearby Cinema lat && lng  according to google map api

--- FIT5140FinalApp/Cinema/CinemaViewController.swift
+++ FIT5140FinalApp/Cinema/CinemaViewController.swift
@@ -9,37 +9,44 @@ import UIKit
 import MapKit
 
 class CinemaViewController: UIViewController {
-
+    
     @IBOutlet weak var mapView: MKMapView!
     var latitudeCollection:[Double] = [Double]()
     var longitudeCollection:[Double] = [Double]()
-    
-    var geometry:[Geometry] = [Geometry]()
     override func viewDidLoad() {
         super.viewDidLoad()
-       mapView.delegate = self
+        mapView.delegate = self
         // Do any additional setup after loading the view.
         
-     if latitudeCollection.count > 0 {
         
-         print("latitudeCollection\(latitudeCollection.count)")
-     }else{
-        print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-        }
-        
-        print("4444444444!\(geometry.count)")
-    
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        getMovieDetailAccordingToMovieId(lat: -37.8828617, log: 145.0913041)
+        getCinemaAccordingToUserCurrentLocation(lat: -37.8828617, log:145.0913041)
+    self.perform(#selector(latitudeCollectionCount), with: nil, afterDelay: 3.0)
+        
+    }
+    
+   @objc func  latitudeCollectionCount(){
+        var id:Int = 1
+        
+        if self.latitudeCollection.count > 0 {
+            
+            print("%%%%%%%%%%\(self.latitudeCollection.count)")
+        }
+        
+        for test  in self.latitudeCollection{
+           print("loop: \(test)")
+           print("count!!: \(id)")
+            id = id+1
+            
+        }
     }
     
     
-    
-    func getMovieDetailAccordingToMovieId(lat:Double,log:Double){
-        self.latitudeCollection = [Double]()
-        self.longitudeCollection = [Double]()
+    func getCinemaAccordingToUserCurrentLocation(lat:Double,log:Double){
+        //self.latitudeCollection = []
+        
         let searchString = Constants.MAP_REQUEST + "\(lat),\(log)&radius=15000&type=movie_theater&key="+Constants.MAP_KEY
         
         let jsonURL = URL(string: searchString.addingPercentEncoding(withAllowedCharacters:
@@ -55,19 +62,21 @@ class CinemaViewController: UIViewController {
             do{
                 let decoder = JSONDecoder()
                 let objectData = try decoder.decode(nearByCinema.self, from: data!)
-                var i:Int = 1
                 if let values = objectData.results{
-                    for value in values{
-                        self.latitudeCollection.append(value.geometry!.location!.lat!)
-                        self.longitudeCollection.append(value.geometry!.location!.lng!)
-                        self.geometry.append(value.geometry!)
-                    print("Count \(i)")
-                           i = i+1;
-                        print("name=  \(value.name!)")
-                    }
                     
+                    for value in values{
+                        let latResult: Double = value.geometry!.location!.lat!
+                        print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+                        print("test : \(latResult)")
+                        print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+                        print("")
+                        
+                        self.latitudeCollection.append(latResult)
+                        self.longitudeCollection.append(value.geometry!.location!.lng!)
+                        print("##### \(self.latitudeCollection.count)")
+                        
+                    }
                 }
-               
                 
             }catch let error{
                 print("!!!!!!!!!!!!!"+error.localizedDescription)
@@ -79,9 +88,9 @@ class CinemaViewController: UIViewController {
         
     }
     
-
-   
-
+    
+    
+    
 }
 
 extension CinemaViewController: MKMapViewDelegate{
